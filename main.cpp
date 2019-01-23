959438f04695c6e990c401a373585903b546ed5f
Gu Junhua <jhgu@nao.cas.cn>
2019-01-23 22:23:21 +0800

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -6,28 +6,30 @@
 using namespace std::chrono_literals;
 
 int main(int argc,char* argv[]){
+
+    //generate a buffer queue with two buffer blocks
     BufQ<int> q{std::make_shared<int>(4), std::make_shared<int>(0)};
 
     int cnt=0;
+    //spawn a thread which is responsible to fill 
+    //valid data into the buffers.
     std::thread th([&](){
         while(1){
             q.write([&](auto x){
+                //sleep for a while to emulate
+                //expensive computings.
                 std::this_thread::sleep_for(1s);
                 *x=cnt++;
-                //std::cout<<":"<<*x<<std::endl;
             });
-            std::this_thread::yield();
-            //std::this_thread::sleep_for(0.1s);
         }
     });
 
+    //main thread that consumes the buffer
     while(1){
-        //std::this_thread::sleep_for(2.5s);
         auto a=(*q.fetch());
         std::cout<<"main:"<<a<<std::endl;
     }
-
-
+    
     th.join();
     return 0;
 }
